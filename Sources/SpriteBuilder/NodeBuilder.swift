106f4227bf888af6498d6ccbe595466d362475a0
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-12-07 21:10:47 -0600

    moved fillcolor modifier

--- Sources/SpriteBuilder/NodeBuilder.swift
+++ Sources/SpriteBuilder/NodeBuilder.swift
@@ -58,15 +58,3 @@ public struct Name: Modifier{
 	}
 }
 
-public struct FillColor: Modifier{
-	public let color: GameColor
-	public init(_ color: GameColor){
-		self.color = color
-	}
-	public func modify(node: SKNode) {
-		if let shape = node as? SKShapeNode {
-			shape.fillColor = color.skColor
-		}
-	}
-}
-
