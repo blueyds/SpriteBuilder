ebbb1f8570e9289c2eb4a5932f7a337bd1c924ff
Blueyds <9055449+blueyds@users.noreply.github.com>
2025-01-08 20:09:41 -0600

    error

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SpriteBuilder", 
-			dependencies: ["GameColor"]), 
+			dependencies: ["SwiftGameColor"]), 
         .testTarget(
             name: "SpriteBuilderTests",
-            dependencies: ["SpriteBuilder", "GameColor"]),
+            dependencies: ["SpriteBuilder", "SwiftGameColor"]),
     ]
 )
